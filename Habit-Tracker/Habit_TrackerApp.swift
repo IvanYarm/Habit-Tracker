8d2b01f775f4ac6853a68a14d33c6be71f4181a0
Ivan <ivan.yarmoliuk@gmail.com>
2023-06-25 16:12:01 -0700

    Initial Commit

---
+++ Habit-Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit-Tracker
+//
+//  Created by Ivan Yarmoliuk on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
